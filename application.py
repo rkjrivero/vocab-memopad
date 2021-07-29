3ce35459e47d7064bc45c198d849de3526ebb766
rkj Rivero <65072768+rkjrivero@users.noreply.github.com>
2021-07-29 20:23:04 +0800

    commented out problematic print test

--- application.py
+++ application.py
@@ -210,7 +210,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(usertable) != 1 or not check_password_hash(usertable[0]["hash"], request.form.get("password")):
-            print("test, check_password_hash: ",check_password_hash(usertable[0]["hash"], request.form.get("password")) )
+            #print("test, check_password_hash: ",check_password_hash(usertable[0]["hash"], request.form.get("password")) )
             return apology("invalid username and/or password", 403)
 
         # Establish session[] array based on "user" table on database
